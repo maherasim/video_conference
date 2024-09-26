22a36aaedb4eb8b16bcd6e760f67e65ee19aba4e
Asim Riaz <asimriazasim107@gmail.com>
2024-09-26 10:13:12 +0500

    clamed call

--- claimcall.php
+++ claimcall.php
@@ -146,9 +146,8 @@ function sendWebSocketNotification($call_id , $support_id) {
         $message = json_encode([
             'action' => 'claim_call',
             'call_id' => $call_id,
-             
-            
-            'support id' => $support_id,
+            'support_name' => $support_id,
+            'support_id' => $support_id,
         ]);
 
         // Log the WebSocket message before sending
